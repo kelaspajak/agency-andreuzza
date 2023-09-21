37d5a41738ec8b41b55bd62d882f25e5fed6a68e
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2023-09-21 10:30:48 +0300

    Initial commit

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+const menu_btn = document.getElementById('menu_btn')
+const mobile_menu = document.getElementById('mobile_menu')
+const menu_bars = document.getElementById('menu_bars')
+const menu_close = document.getElementById('menu_close')
+
+menu_btn.addEventListener('click', () => {
+  mobile_menu.classList.toggle('hidden')
+  menu_bars.classList.toggle('hidden')
+  menu_close.classList.toggle('hidden')
+})
