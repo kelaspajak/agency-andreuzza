374d35e47052d698c8b72e7a3fdc90b866f42048
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-10-04 18:58:36 +0300

    added nav and footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,29 +4,29 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-
     extend: {
-
       colors: {
-
-        blue: {
-         50: "#EBEBFF",
-      100: "#D2D2FE",
-      200: "#A6A4FE",
-      300: "#7E7CFD",
-      400: "#524FFD",
-      500: "#2522FC",
-      600: "#0703E2",
-      700: "#0502AB",
-      800: "#03026F",
-      900: "#020137",
-      950: "#01001E"
+        accent: {
+          '50': 'hsl(9, 100%, 97%)',
+    '100': 'hsl(8, 100%, 94%)',
+    '200': 'hsl(8, 100%, 89%)',
+    '300': 'hsl(9, 100%, 81%)',
+    '400': 'hsl(9, 100%, 70%)',
+    '500': 'hsl(9, 96%, 60%)',
+    '600': 'hsl(9, 82%, 53%)',
+    '700': 'hsl(8, 84%, 42%)',
+    '800': 'hsl(8, 80%, 35%)',
+    '900': 'hsl(9, 72%, 31%)',
+    '950': 'hsl(9, 85%, 15%)',
         },
       },
       fontFamily: {
-        display: ["Newsreader", ...defaultTheme.fontFamily.sans],
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+        sans: ["Bricolage Grotesque", ...defaultTheme.fontFamily.sans],
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3rem",
+        "6xl": "5rem",
       },
     },
   },
